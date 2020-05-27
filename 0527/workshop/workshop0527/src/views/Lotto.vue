e870fa6e2ea189d2545cfc84f60d6cb350b9832e
gkgg123 <gkgg123@naver.com>
2020-05-27 18:19:20 +0900

    과제

---
+++ 0527/workshop/workshop0527/src/views/Lotto.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>로또 추첨</h1>
+    <button @click="pick">추천</button>
+    <p>{{lotto_number}}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+
+  data : function () {
+    return {
+      numbers : _.range(1,46),
+      lotto_number : '',
+    }
+      
+    },
+  methods :{
+    pick : function(){
+      this.lotto_number=_.sampleSize(this.numbers,6).sort(function(a,b){return a-b})
+    }
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 0527/workshop/workshop0527/src/views/Lunch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>점심 메뉴</h1>
+    <button @click="pick">메뉴 추천</button>
+    <p>{{menu}}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+data : function (){
+  return {
+    menus : ['짜장','고기','김치찌개','남다른감자탕','매드포갈릭'],
+    menu : '',
+  }
+},
+methods :{
+  pick : function(){
+    this.menu = _.sample(this.menus)
+  }
+}
+
+}
+</script>
+
+<style>
+
+</style>
